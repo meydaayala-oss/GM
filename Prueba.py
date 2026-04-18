1ea5203c8e95a3ffdf37c327d571d0c24a4768a6
Daniela <meybelindanielaguerreroayala@gmail.com>
2026-04-18 11:57:12 -0600

    Acualizacion

--- Prueba.py
+++ Prueba.py
@@ -1,19 +1,1 @@
-print("lalalahgugugugl")
-print("lalalalaal")
-print("lalalalaal")
-print("lalalalaal")
-print("lalalalaal")
-print("lalalalaal")
-print("lalalalaal")
-
-print("aver")
-
-print ("no me convences")
-
-print("esta vaina es seria")
-print("esta vaina es seria")
-
-print("cansado jefe")
-
-print("esta vaina es seria")
 
